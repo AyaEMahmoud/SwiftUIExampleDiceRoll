fad64d4d24124889895c289c6c3091adb278d060
Aya Mahmoud <ayamahmoud@Ayas-MacBook-Pro.local>
2023-12-10 06:27:08 +0200

    Intial commit, added assets

---
+++ SwiftUIExampleDiceRoll/SwiftUIExampleDiceRollApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIExampleDiceRollApp.swift
+//  SwiftUIExampleDiceRoll
+//
+//  Created by Aya Mahmoud on 10/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIExampleDiceRollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
