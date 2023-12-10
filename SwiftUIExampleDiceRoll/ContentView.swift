c84d436ead007a513af06b0e113e2a2102c996a4
Aya Mahmoud <ayamahmoud@Ayas-MacBook-Pro.local>
2023-12-10 06:46:08 +0200

    Add intial design

--- SwiftUIExampleDiceRoll/ContentView.swift
+++ SwiftUIExampleDiceRoll/ContentView.swift
@@ -9,18 +9,45 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            
+            Image("background")
+                .resizable()
+                .ignoresSafeArea()
+            
+            VStack {
+                Image("diceeLogo")
+                HStack {
+                    DiceView(n: 1)
+                    DiceView(n: 4)
+                }
+                Button("Roll") {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                }.foregroundColor(.white)
+                    .padding()
+                    .font(Font.system(size: 50))
+                    .fontWeight(.heavy)
+                    .background(Color.secondary)
+            }
+            
+            
         }
-        .padding()
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct DiceView: View {
+    let n: Int
+    var body: some View {
+        Image("dice\(n)")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .padding()
     }
 }
